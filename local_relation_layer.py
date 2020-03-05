3b20d75f842fc02b179163f2a8d1af72695b583f
unknown <156844521@qq.com>
2020-03-05 11:35:29 +0800

    first commit

--- local_relation_layer.py
+++ local_relation_layer.py
@@ -1,5 +1,6 @@
 import torch
 
+
 class GeometryPrior(torch.nn.Module):
     def __init__(self, k, channels, multiplier=0.5):
         super(GeometryPrior, self).__init__()
@@ -10,10 +11,17 @@ class GeometryPrior(torch.nn.Module):
         
     def forward(self):
         # as the paper does not infer how to construct a 2xkxk position matrix
-        # we replace it as a random matrix
-        position = torch.rand(1, 2, self.k, self.k, requires_grad=True)
+        # we assume that it's a kxk matrix for deltax,and a kxk matric for deltay.
+        # that is, [[[-1,0,1],[-1,0,1],[-1,0,1]],[[1,1,1],[0,0,0],[-1,-1,-1]]] for kernel = 3
+        a_range  = torch.arange(-1*(self.k//2),(self.k//2)+1).view(1,-1)
+        x_position = a_range.expand(self.k,a_range.shape[1])
+        b_range = torch.arange((self.k//2),-1*(self.k//2)-1,-1).view(-1,1)
+        y_position = b_range.expand(b_range.shape[0],self.k)
+        position = torch.cat((x_position.unsqueeze(0),y_position.unsqueeze(0)),0).unsqueeze(0).float()
+        if torch.cuda.is_available():
+            position = position.cuda()
         out = self.l2(torch.nn.functional.relu(self.l1(position)))
-        return out.view(1, self.channels, self.k, self.k)
+        return out
 
 
 
@@ -35,16 +43,12 @@ class AppearanceComposability(torch.nn.Module):
     def forward(self, x):
         key_map, query_map = x
         k = self.k
-        key_map_unfold = self.unfold(key_map)   # [N batch , C channel * k*k, H_out*Wout]
-        query_map_unfold = self.unfold(query_map)    # [N batch , C channel * k*k, H_out*Wout]
-        key_map_unfold = key_map_unfold.view(
-                    key_map.shape[0], key_map.shape[1],
-                    -1,
-                    key_map_unfold.shape[-2] // key_map.shape[1])   # [N batch, C/m channel, H_out*Wout), k*k]
-        query_map_unfold = query_map_unfold.view(
-                    query_map.shape[0], query_map.shape[1],
-                    -1,
-                    query_map_unfold.shape[-2] // query_map.shape[1])
+        key_map_unfold = self.unfold(key_map).transpose(2,1).contiguous()   # [N batch , H_out*Wout, C channel * k*k]
+        query_map_unfold = self.unfold(query_map).transpose(2,1).contiguous()    # [N batch , H_out*Wout, C channel * k*k]
+        key_map_unfold = key_map_unfold.view(key_map.shape[0],-1, key_map.shape[1], key_map_unfold.shape[-1]//key_map.shape[1])
+        query_map_unfold = query_map_unfold.view(query_map.shape[0], -1, query_map.shape[1], query_map_unfold.shape[-1]//query_map.shape[1])
+        key_map_unfold = key_map_unfold.transpose(2,1).contiguous()
+        query_map_unfold = query_map_unfold.transpose(2,1).contiguous()
         return (key_map_unfold * query_map_unfold[:, :, :, k**2//2:k**2//2+1]).view(key_map_unfold.shape[0],key_map_unfold.shape[1],key_map_unfold.shape[2],k,k)    #[N batch, C channel, (H-k+1)*(W-k+1), k*k]
 
 
@@ -53,19 +57,13 @@ def combine_prior(appearance_kernel, geometry_kernel):
 
 
 class LocalRelationalLayer(torch.nn.Module):
-    def __init__(self, channels, k, stride=1, m=None):
-    '''
-    channels: channels of input
-    k: kernel size
-    stride: stride
-    m: parameter of channel sharing
-    '''
+    def __init__(self, channels, k, stride=1, padding =0,m=None):
         super(LocalRelationalLayer, self).__init__()
         self.channels = channels
         self.k = k
         self.stride = stride
         self.m = m or 8
-        self.padding = self.k//2
+        self.padding = padding
         self.kmap = KeyQueryMap(channels, self.m)
         self.qmap = KeyQueryMap(channels, self.m)
         self.ac = AppearanceComposability(k, self.padding, self.stride)
@@ -74,12 +72,12 @@ class LocalRelationalLayer(torch.nn.Module):
         self.final1x1 = torch.nn.Conv2d(channels, channels, 1)
         
     def forward(self, x):   # x = [N,C,H,W]
-        km = self.kmap(x)       # [C/m,h,w]
-        qm = self.qmap(x)       # [C/m,h,w]
-        ak = self.ac((km, qm))  # [N,C/mm,H_out*W_out, k,k]
-        gpk = self.gp()    # [ C/m,k,k]
-        ck = combine_prior(ak, gpk.unsqueeze(2))[:, None, :, :, :]  # [N,1,C/8,H_out*W_out, k,k]
-        x_unfold = self.unfold(x)
+        km = self.kmap(x)       # [N,C/m,h,w]
+        qm = self.qmap(x)       # [N,C/m,h,w]
+        ak = self.ac((km, qm))  # [N,C/m,H_out*W_out, k,k]
+        gpk = self.gp()    # [1, C/m,k,k]
+        ck = combine_prior(ak, gpk.unsqueeze(2))[:, None, :, :, :]  # [N,1,C/m,H_out*W_out, k,k]
+        x_unfold = self.unfold(x).transpose(2,1).contiguous().view(x.shape[0], -1, x.shape[1], self.k*self.k).transpose(2,1).contiguous()
         x_unfold = x_unfold.view(x.shape[0], self.m, x.shape[1] // self.m, -1, self.k, self.k)     # [N, m, C/m, H_out*W_out, k,k]
         pre_output = (ck * x_unfold).view(x.shape[0], x.shape[1], -1, self.k*self.k)     #  [N, C,HOUT*WOUT, k*k]
         h_out = (x.shape[2] + 2 * self.padding - 1 * self.k )//  self.stride + 1
@@ -89,6 +87,6 @@ class LocalRelationalLayer(torch.nn.Module):
 
 if __name__ == '__main__':
     # example of local relation layer
-    layer = LocalRelationalLayer(channels=64,k=7,stride=1,m=8)
-    input = torch.zeros(2,64,19,19)
+    layer = LocalRelationalLayer(channels=64,k=7,stride=1,padding=0, m=8).cuda()
+    input = torch.zeros(2,64,19,19).cuda()
     output = layer(input)
